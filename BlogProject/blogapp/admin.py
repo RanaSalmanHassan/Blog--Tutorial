f61793e5559f29a073c616549eb63888d099a9a1
Salman <ranasalmanhassan2007@gmail.com>
2023-04-13 16:41:53 +0500

    Comment Styled

--- BlogProject/blogapp/admin.py
+++ BlogProject/blogapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Blog_Model
+from .models import Blog_Model,Comment
 # Register your models here.
 admin.site.register(Blog_Model)
+admin.site.register(Comment)
--- BlogProject/blogapp/forms.py
+++ BlogProject/blogapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Blog_Model,Comment
+from django.forms import ModelForm, TextInput, EmailInput
 
 class Create_Blog_Form(forms.ModelForm):
     class Meta:
@@ -10,3 +11,10 @@ class Create_Comment_Form(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ('user','blog',)
+
+        widgets = {
+            'comment': TextInput(attrs={
+                'style': 'border: none; border-bottom:1px solid gray; width:300px;',
+                'placeholder': 'Enter Commment',
+
+                })}
